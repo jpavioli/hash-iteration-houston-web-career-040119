eed5fd8de7174d699e721389729ac6dd70803897
Jusitn Avioli <justin.avioli@gmail.com>
2019-03-27 18:37:40 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -9,8 +9,7 @@ def happy_birthday(birthday_kids)
   birthday_kids.each do |kid,age|
     if age < 12
       puts "Happy Birthday #{kid}! You are now #{age} years old!"
-    else
-      puts "You are too old for this."
+
     end
   end
 end
